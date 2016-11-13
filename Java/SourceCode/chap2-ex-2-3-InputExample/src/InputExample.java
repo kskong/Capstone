604516a0b5602e798b61a13e9d1120c676894f6b
kskong <kskong@kpu.ac.kr>
2016-11-13 23:30:46 +0900

    GitHub Desktop 테스트 파일 복사
    Git/GitHub 강의 자료, Java 강의자료 및 소스코드

---
+++ Java/SourceCode/chap2-ex-2-3-InputExample/src/InputExample.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class InputExample {
+	public static void main (String args[]) { 
+		InputStreamReader rd = new InputStreamReader(System.in);
+		try {
+			while (true) {
+				int a = rd.read();
+				if (a == -1)	// ctrl-z가 입력되면 read()는 -1을 리턴
+					break;
+				System.out.println((char)a);	// 입력된 문자 출력
+			}  
+		} 
+		catch (IOException e) {
+			System.out.println("입력 오류 발생");
+		}
+	}
+}
